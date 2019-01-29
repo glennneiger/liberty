087eb2313a3b5622b199de8e80a107d017b2afa3
surajchandrasekhar <surajchandrasekhar96@gmail.com>
2019-01-29 12:20:21 -0800

    Add files via upload

--- index.py
+++ index.py
@@ -10,12 +10,21 @@ app = Flask(__name__)
 
 # initialize Pusher
 pusher_client = pusher.Pusher(
-    app_id='697236',
-    key='55eb42c4cc7279f47304',
-    secret='2a3b335f600825d3f2ef',
-    cluster='us2',
-    ssl=True
+  app_id='701796',
+  key='92992f27c9efddd649df',
+  secret='c23ec3c8895c227e0128',
+  cluster='us2',
+  ssl=True
 )
+
+# Employee Database holder
+employee_db = {'12345': ['robert', 'robert@2019', 'Robert'], '12346': ['arthur', 'arthur@2019', 'Arthur'], '12347': ['cobb', 'cobb@2019', 'Cobb'],
+               '12348': ['bruce', 'bruce@2019', 'Bruce'], '12349': ['ariadne', 'ariadne@2019', 'Ariadne']}
+
+# Main Category to Sub Categories holder
+categories = {'Disciplinary': ['Drugs and Violence', 'Other Disciplinary Actions'], 'Behavior': ['Outside of Work', 'Business Conduct', 'Professionality'],
+            'Benefits': ['Medical Benefits', 'Other Benefits'], 'General': ['Introduction to Company', 'Hours', 'Company Guidelines', 'Work'],
+            'Salary': ['Refunds and Deductions', 'Pay']}
 
 
 @app.route('/')
@@ -23,58 +32,75 @@ def index():
     return render_template('index.html')
 
 
-# @app.route('/get_movie_detail', methods=['POST'])
-# def get_movie_detail():
-#     data = request.get_json(silent=True)
-#
-#     try:
-#         movie = data['queryResult']['parameters']['movie']
-#         api_key = '85dae30f'
-#
-#         movie_detail = requests.get('http://www.omdbapi.com/?t={0}&apikey={1}'.format(movie, api_key)).content
-#         movie_detail = json.loads(movie_detail)
-#
-#         response = """
-#             Title : {0}
-#             Released: {1}
-#             Actors: {2}
-#             Plot: {3}
-#         """.format(movie_detail['Title'], movie_detail['Released'], movie_detail['Actors'], movie_detail['Plot'])
-#     except:
-#         response = "Could not get movie detail at the moment, please try again"
-#
-#     reply = {
-#         "fulfillmentText": response,
-#     }
-#
-#     return jsonify(reply)
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+@app.route('/get_login_detail', methods=['POST'])
+def get_login_detail():
+    data = request.get_json(silent=True)
+    response = ""
+    try:
+        if data['queryResult']['action'] == 'employee_username':
+            print('Check 1')
+            response = "Username and Password do not match, what is your username?"
+            userid = data['queryResult']['parameters']['user_id']
+            password = data['queryResult']['parameters']['password']
+            print("Username data type: ", type(userid))
+            print("Password data type: ", type(password))
+            print('User ID: ', userid, 'Password: ', password)
+            print("Expected Password: ", employee_db[userid][1])
+            if(userid in employee_db.keys()):
+                if(employee_db[userid][1]==password):
+                    print("Yes")
+                    response = "Welcome {0}, what category would you like to find out more about today?".format(employee_db[userid][2])
+        if data['queryResult']['action'] == 'MainCat':
+            print('check 2')
+            response = "Category doesn't exist, choose another one"
+            maincat = data['queryResult']['parameters']['maincat']
+            if(maincat in categories.keys()):
+                subs = ''
+                for i in range(0, len(categories[maincat])-1):
+                    subs += categories[maincat][i] + ', '
+                subs += 'or '+categories[maincat][len(categories[maincat])-1]
+                response = 'Under {0}, would you like to find out more about {1}?'.format(maincat, subs)
+
+    except:
+        response = ""
+
+    reply = {
+        "fulfillmentText": response
+    }
+
+    return jsonify(reply)
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
+    print("Check")
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
-
     if text:
         text_input = dialogflow.types.TextInput(
             text=text, language_code=language_code)
         query_input = dialogflow.types.QueryInput(text=text_input)
         response = session_client.detect_intent(
             session=session, query_input=query_input)
-
         return response.query_result.fulfillment_text
 
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
-    #   socketId = request.form['socketId']
     message = request.form['message']
+    print(message)
     project_id = 'liberty-735ff'
     fulfillment_text = detect_intent_texts(project_id, "unique", message, 'en')
     response_text = {"message": fulfillment_text}
-
+    socketId = request.form['socketId']
     pusher_client.trigger('liberty', 'new_message',
-                          {'human_message': message, 'bot_message': fulfillment_text})
-
+                          {'human_message': message, 'bot_message': fulfillment_text}, socketId)
+    print(fulfillment_text)
+    print(response_text)
     return jsonify(response_text)
 
 
